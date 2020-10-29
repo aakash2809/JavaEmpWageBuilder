73341dc7fb832cfb1530e6cdf9831746c9e7b959
aakash2809 <aakashrajak289@gmail.com>
2020-10-29 08:07:47 +0530

    Add : compute wage for month

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,40 +1,58 @@
 public class  EmpWageBuilder{
-	static int IS_FULL_TIME = 1;// constants
-   static int WAGE_PER_HOUR = 20;// wage per hour
-   static int FULL_DAY_HOUR = 8; // Number of fixed Hours per day for full day
-   static int PART_TIME_HOUR = 4; // Number of fixed Hours per day for part time
-   static double calculateDailyWage = 0;
+	static int WAGE_PER_HOUR = 20; // wage per hour
+	static int FULL_DAY_HOUR = 8; // Number of fixed Hours per day for full day
+	static int PART_TIME_HOUR = 4;// Number of fixed Hours per day for part time
+	static int Working_DAYS_PER_MONTH = 20;//
+	static double calculateDailyWage = 0;// vairable for total wage calculation
+	static int totalWorkingDays = 0;// total working days
 
-   //function to give result for no.of hours done for work done
-   public static int getWorkPerDay(int employeeStatus) {
-      int workDone = 0;
-      switch (employeeStatus) {
-      case 0:
-         workDone = FULL_DAY_HOUR;
-         break;
-      case 1:
-         workDone = PART_TIME_HOUR;
-         break;
+	// function to give result for no.of hours done for work done
+	public static int getWorkPerDay(int employeeStatus) {
+		int workDone = 0;
+		switch (employeeStatus) {
+		case 0:
+			workDone = PART_TIME_HOUR;
+			break;
+		case 1:
+			workDone = FULL_DAY_HOUR;
+			break;
+		}
+		return workDone;
+	}
 
-      }
-      return workDone;
-   }
+	// main function
+	public static void main(String args[]) {
 
-   // main function
-   public static void main(String args[]) {
+		int numberOfHalfDays = 0;
+		int numberOfFulldays = 0;
+		int count = 0;
 
-      System.out.println("Welcome to Employee wage Computation Program");
-      double empCheck = Math.floor(Math.random() * 10) % 2;// computation
-      if (empCheck == IS_FULL_TIME) // To check Employee is present or not
-         System.out.println("Employee is Present");
-      else
-         System.out.println("Employee is not Present");
+		System.out.println("Welcome to Employee wage Computation Program");
 
-      int employeeStatus = (int) (Math.floor(Math.random() * 10) % 2); // key genration for half day or full day
-      int dayHours = EmpWageBuilder.getWorkPerDay(employeeStatus);
+		// loop till working days per month
+		while (count < Working_DAYS_PER_MONTH) {
+			int employeeStatus = (int) (Math.floor(Math.random() * 10) % 2); // key genration for half day or full day
 
-      calculateDailyWage = (dayHours * WAGE_PER_HOUR); // daily full day wage camputation
-      System.out.println("Employee daily  day wage according to full day or half day - " + calculateDailyWage);
+			// counting number of full days and half days
+			if (employeeStatus == 0)
+				numberOfHalfDays++;
+			else
+				numberOfFulldays++;
 
-   }
+			int dayHours = EmpWageBuilder.getWorkPerDay(employeeStatus);
+			calculateDailyWage = calculateDailyWage + (dayHours * WAGE_PER_HOUR);
+			count++;
+
+		}
+
+		totalWorkingDays = numberOfFulldays + numberOfHalfDays;// total working days computation
+
+		System.out.println("total half working days - " + numberOfHalfDays);
+		System.out.println("total full working days - " + numberOfFulldays);
+		System.out.println("total working days      - " + totalWorkingDays);
+		System.out.println("total wage              - " + calculateDailyWage);
+
+	}
+
+
 }
