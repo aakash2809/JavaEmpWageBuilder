76178ca67dae81d269f1d98a932227c39affe231
aakash2809 <aakashrajak289@gmail.com>
2020-10-29 22:41:30 +0530

    Wage Computation for Many Companies

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,17 +1,25 @@
 public class EmpWageBuilder {
-	static int WAGE_PER_HOUR = 20; // WAGE PER HOUR
-	static int FULL_DAY_HOUR = 8; //NUMBER OF FIXED HOURS PER DAY FOR FULL DAY 
-	static int PART_TIME_HOUR = 4;//NUMBER OF FIXED HOURS PER DAY FOR HALF DAY OR PART TIME
-	static int Working_DAYS_PER_MONTH = 20;// MAXIMUM NO OF WORKING DAYS PER MONTH
-	static int WORKING_hOURS_PER_MONTH = 100;//MAXIMUM NUMBER OF HOURS PER MONTH
-	static int workigHoursCompleted = 0; //NUMBER OF HOURS COMPLETED BY EMPLOYEE
-	static int EMPLOYEE_STATUS = 1; // CONSTANT TO CHECK WHETHER EMPLOYEE PRESENT OF ABSENT
-	static double calculateDailyWage = 0;//VARIABLE FOR TOTAL WAGE 
-	static int workigDaysCompleted = 0; //NUMBER OF DAYS COMPLETED BY EMPLOYEE
-	static int numberOfHalfDays = 0; //STORES NUMBER OF HALF DAYS
-	static int numberOfFulldays = 0;//STORES NUMBER OF FULL DAYS
-	static int numberofPresent = 0;//NUMBER OF PRESNTS
-	static int numberOfAbsent = 0;//NNUMBBER OF ABSENTS
+	
+	private static final int PART_TIME_HOUR =4;//NUMBER OF FIXED HOURS PER DAY FOR HALF DAY OR PART TIME
+	private static final int FULL_DAY_HOUR = 8;//NUMBER OF FIXED HOURS PER DAY FOR FULL DAY 
+	private static final int EMPLOYEE_STATUS = 1; // CONSTANT TO CHECK WHETHER EMPLOYEE PRESENT OF ABSENT
+	private String company;//COMPANYNAME
+	private int Working_DAYS_PER_MONTH;// MAXIMUM NO OF WORKING DAYS PER MONTH
+	private int WAGE_PER_HOUR;// WAGE PER HOUR
+	private int WORKING_HOURS_PER_MONTH;//MAXIMUM NUMBER OF HOURS PER MONTH
+	
+	//DEFAULT CONSTRUCTOR
+	public EmpWageBuilder() {
+
+	}
+	
+	//CONSTRUCTOR FOR ASSIGNING WAGE DATA FOR SPECIFIC COMPANY
+	public EmpWageBuilder(String company, int working_DAYS_PER_MONTH,int WORKING_HOURS_PER_MONTH, int WAGE_PER_HOUR) {
+		this.company = company;
+		this.Working_DAYS_PER_MONTH = working_DAYS_PER_MONTH;
+		this.WAGE_PER_HOUR = WAGE_PER_HOUR;
+		this.WORKING_HOURS_PER_MONTH = WORKING_HOURS_PER_MONTH;
+	}
 
 	// FUNCTION TO GIVE RESULT HOW MANY HOURS HE WORKS BASED ON RANDOM KEY FOR HALF DAY OR FULL DAY
 	public static int getWorkPerDay(int employeeStatus) {
@@ -29,54 +37,65 @@ public class EmpWageBuilder {
 	}
 	
 	//CALUCLATE EMPLOYEE MONTHLY WAGE ON THE BASES OF HIS ATTANDANCE STATUS
-	public static double computeEmpWage(int empAttandanceStatus) {
-		if (empAttandanceStatus == EMPLOYEE_STATUS) {
-			
-			numberofPresent++;
-			
-			int empDayStatus = (int) (Math.floor(Math.random() * 10) % 2); // KEY GENRATION FOR HALF DAYS AND FULL DAYS
-																	
-			// TO COUNT NO OF FULL DAYS AND HALF DAYS
-			if (empDayStatus == 0)
-				numberOfHalfDays++;
-			else
-				numberOfFulldays++;
-
-			int dayHours = EmpWageBuilder.getWorkPerDay(empDayStatus);
-			workigHoursCompleted=workigHoursCompleted+dayHours;
-			
-			if(workigHoursCompleted>100) {
-				workigHoursCompleted=100;
-			}
-			calculateDailyWage = calculateDailyWage + (dayHours * WAGE_PER_HOUR);
-		} 
-
-		else
-			numberOfAbsent++;
-		
-		return calculateDailyWage;
-		
-	}
-
-	// MAIN FUNCTION
-	public static void main(String args[]) {
-		System.out.println("Welcome to Employee wage Computation Program ");
+	public void computeEmpWage() {
+		int workigHoursCompleted = 0;
+		int workigDaysCompleted = 0;
+		int numberOfPresent = 0;
+		int numberOfAbsent = 0;
+		int numberOfHalfDays = 0;
+		int numberOfFulldays = 0;
+		int calculateDailyWage=0;
+		System.out.println("Welcome to Employee wage Computation Program");
 		System.out.println("--------------------------------------------------");
+		
 		// LOOP TILL CONDITION DID NOT REACH FOR TERMINATION
-		while (workigDaysCompleted < Working_DAYS_PER_MONTH && workigHoursCompleted < WORKING_hOURS_PER_MONTH) {
+		while (workigDaysCompleted < Working_DAYS_PER_MONTH && workigHoursCompleted < WORKING_HOURS_PER_MONTH) {
 			int empAttandanceStatus = (int) (Math.floor(Math.random() * 10) % 2);
-			calculateDailyWage=EmpWageBuilder.computeEmpWage(empAttandanceStatus);
-			workigDaysCompleted++;
-	   }
+			
+			if (empAttandanceStatus == EMPLOYEE_STATUS) {
+				
+				numberOfPresent++;
+				int empDayStatus = (int) (Math.floor(Math.random() * 10) % 2); // KEY GENRATION FOR HALF DAYS AND FULL DAYS
+																		
+				// TO COUNT NO OF FULL DAYS AND HALF DAYS
+				if (empDayStatus == 0)
+					numberOfHalfDays++;
+				else
+					numberOfFulldays++;
 
-		System.out.println("Number of days presents  - " + numberofPresent);
+				int dayHours = EmpWageBuilder.getWorkPerDay(empDayStatus);
+				workigHoursCompleted=workigHoursCompleted+dayHours;
+				
+				if(workigHoursCompleted>this.WORKING_HOURS_PER_MONTH) {
+					workigHoursCompleted=this.WORKING_HOURS_PER_MONTH;
+				}
+			calculateDailyWage = calculateDailyWage + (dayHours * WAGE_PER_HOUR);
+			} 
+
+			else
+				numberOfAbsent++;
+			
+			workigDaysCompleted++;
+			
+			}
+		
+		System.out.println("Your Company Name        - " +this.company);
+		System.out.println("Number of days presents  - " + numberOfPresent);
 		System.out.println("total of days absents    - " + numberOfAbsent);
 		System.out.println("total half working days  - " + numberOfHalfDays);
 		System.out.println("total full working days  - " + numberOfFulldays);
 		System.out.println("Number of hours completed- " +workigHoursCompleted );
 		System.out.println("total wage               - " + calculateDailyWage);
+	}
+	
+	public static void main(String[] args) {
+	
+		EmpWageBuilder Dmart=new EmpWageBuilder("Dmart",20,100,20);
+		EmpWageBuilder Airtel=new EmpWageBuilder("Airtel",40,200,30);
+		Dmart.computeEmpWage();
+		Airtel.computeEmpWage();
+	}
 
-  }
 }
 
 
