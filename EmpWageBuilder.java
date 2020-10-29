4855b61a10bd2714686782a93e79c8e23963d0a8
aakash2809 <aakashrajak289@gmail.com>
2020-10-29 07:16:18 +0530

    Add : Part Time Employee Wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,11 +1,12 @@
 public class  EmpWageBuilder{
 		public static void main(String args[]){
-			int IS_FULL_TIME = 1;                                  //constants
+			int IS_FULL_TIME = 1;                                   //constants
          int WAGE_PER_HOUR=20;                           //wage per hour
          int FULL_DAY_HOUR=8;                            //Number of fixed Hours perday
+         int PART_TIME_HOUR=4;
          double calculateDailyWage=0;
 
-         System.out.println("Welcome to Employee Wage Computation Program");
+         System.out.println("Welcome to Employee wage Computation Program");
          double empCheck=Math.floor(Math.random() * 10)%2;//computation
          if(empCheck == IS_FULL_TIME )                   //To check Employee is present or not
             System.out.println("Employee is Present");
@@ -14,7 +15,8 @@ public class  EmpWageBuilder{
 
          calculateDailyWage=(FULL_DAY_HOUR*WAGE_PER_HOUR); //daily full day wage camputation
          System.out.println("Employee daily Full day wage - " +calculateDailyWage);
-
-
+         calculateDailyWage=0;                             // assigning zero for computation  wage for part time
+         calculateDailyWage=(PART_TIME_HOUR*WAGE_PER_HOUR); //daily part time wage camputation
+         System.out.println("Employee daily Part time day wage - " +calculateDailyWage);
 		}
 }
