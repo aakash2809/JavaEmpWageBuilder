1eb69d2b8047138b28e158c441d27bc66b6745c2
aakash2809 <aakashrajak289@gmail.com>
2020-10-29 06:39:07 +0530

    Add : Employee Attandance Status.

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,5 +1,12 @@
 public class  EmpWageBuilder{
 		public static void main(String args[]){
-			System.out.println("Welcome to EmployeeWage ComputationProgram");
+			int IS_FULL_TIME = 1;  //constatnt
+         System.out.println("Welcome to EmployeeWage ComputationProgram");
+         double empCheck=Math.floor(Math.random() * 10)%2; //Employee status computation
+         if(empCheck == IS_FULL_TIME )
+            System.out.println("Employee is Present");
+         else
+             System.out.println("Employee is not Present");
+
 		}
 }
