6277d4931c652c054168608d1cf0020a80c9d7a1
aakash2809 <aakashrajak289@gmail.com>
2020-11-04 11:36:47 +0530

    modified : each company wage by their own logic using interface

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,92 +1,98 @@
-import java.util.ArrayList;
-class CompanyEmpWage {
-	private static final int PART_TIME_HOUR =4;//NUMBER OF FIXED HOURS PER DAY FOR HALF DAY OR PART TIME
-	private static final int FULL_DAY_HOUR = 8;//NUMBER OF FIXED HOURS PER DAY FOR FULL DAY 
-	private static final int EMPLOYEE_STATUS = 1; // CONSTANT TO CHECK WHETHER EMPLOYEE PRESENT OF ABSENT
-	private String company;//COMPANYNAME
-	private int Working_DAYS_PER_MONTH;// MAXIMUM NO OF WORKING DAYS PER MONTH
-	private int WAGE_PER_HOUR;// WAGE PER HOUR
-	private int WORKING_HOURS_PER_MONTH;//MAXIMUM NUMBER OF HOURS PER MONTH
-	
-	//DEFAULT CONSTRUCTOR
-	public CompanyEmpWage() {
+/*
+ *THIS PROGRM SHOWS THE HOW INTERFACE WORKKS 
+ *HERE EACH COMPANY CAN HAVE THEIR OWN DATAT AND THE LOGIC TO CALCULATE EMPLOYEWAGE 
+ *IN PROGRAM IT CAN BE SEE THAT INTERFACE computeEmpWage() METHOD IS OVERRIDE IN DIFFEENT CLASSES AND IMPLEMENTED 
+ *ACCORDIDING INDIVIDUALS OWN LOGIC
+ */
 
-	}
-	
-	//CONSTRUCTOR FOR ASSIGNING WAGE DATA FOR SPECIFIC COMPANY
-	public CompanyEmpWage(String company, int working_DAYS_PER_MONTH,int WORKING_HOURS_PER_MONTH, int WAGE_PER_HOUR) {
-		this.company = company;
-		this.Working_DAYS_PER_MONTH = working_DAYS_PER_MONTH;
-		this.WAGE_PER_HOUR = WAGE_PER_HOUR;
-		this.WORKING_HOURS_PER_MONTH = WORKING_HOURS_PER_MONTH;
-	}
+//INTERFACE 
+interface CompanyEmpWage {
+	public int computeEmpWage();
+}
 
-	// FUNCTION TO GIVE RESULT HOW MANY HOURS HE WORKS BASED ON RANDOM KEY FOR HALF DAY OR FULL DAY
-	public static int getWorkPerDay(int employeeStatus) {
-		int numberOfHours = 0;
-	    switch (employeeStatus) {
-			case 0:
-				   numberOfHours = PART_TIME_HOUR;
-				   break;
-			case 1:
-			       numberOfHours = FULL_DAY_HOUR;
-				   break;
-		}
-					
-	    return numberOfHours;
-	}
-	
-	//CALUCLATE EMPLOYEE MONTHLY WAGE ON THE BASES OF HIS ATTANDANCE STATUS
-	public void computeEmpWage() {
-		int workigHoursCompleted = 0;
-		int workigDaysCompleted = 0;
-		int numberOfPresent = 0;
-		int numberOfAbsent = 0;
-		int numberOfHalfDays = 0;
-		int numberOfFulldays = 0;
-		int calculateDailyWage=0;
-		
-		// LOOP TILL CONDITION DID NOT REACH FOR TERMINATION
-		while (workigDaysCompleted < Working_DAYS_PER_MONTH && workigHoursCompleted < WORKING_HOURS_PER_MONTH) {
-			int empAttandanceStatus = (int) (Math.floor(Math.random() * 10) % 2);
-			if (empAttandanceStatus == EMPLOYEE_STATUS) {
-				
-				numberOfPresent++;
-				int empDayStatus = (int) (Math.floor(Math.random() * 10) % 2); // KEY GENRATION FOR HALF DAYS AND FULL DAYS
-																		
-				// TO COUNT NO OF FULL DAYS AND HALF DAYS
-				if (empDayStatus == 0)
-					numberOfHalfDays++;
-				else
-					numberOfFulldays++;
-
-				int dayHours = CompanyEmpWage.getWorkPerDay(empDayStatus);
-				workigHoursCompleted=workigHoursCompleted+dayHours;
-				
-				if(workigHoursCompleted>this.WORKING_HOURS_PER_MONTH) {
-					workigHoursCompleted=this.WORKING_HOURS_PER_MONTH;
-				}
-			calculateDailyWage = calculateDailyWage + (dayHours * WAGE_PER_HOUR);
-			} 
-
-			else
-				numberOfAbsent++;
-			
-			workigDaysCompleted++;
-			
+//AIRTEL COMPANY HAVING ITS OWN LOGIC AND DATA FOR ITS EMPLOYEE WAGE 
+class Airtel implements CompanyEmpWage {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 2;
+	public static final int MAX_HRS_IN_MONTH = 10;
+	//IMPLEMENTATION DECLARED IN INTERFACE OF COMPPUTE_EMP_WAGE METHOD FOR COMPANY OWN PURPOSE
+	@Override
+	public int computeEmpWage() {
+		// variable
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		System.out.println("            Wellcome to Airtel wage calculater     ");
+		System.out.println("----------------------------------------------------");
+		// COMPUTATION
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
+			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+			switch (empCheck) {
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 4;
 			}
-		 }
-	}
-
-	public class EmpWageBuilder {
-		public static void main(String[] args) {
-		ArrayList<CompanyEmpWage> empWage=new ArrayList<CompanyEmpWage>();
-		CompanyEmpWage Dmart=new CompanyEmpWage("Dmart",20,100,20);
-		CompanyEmpWage Airtel=new CompanyEmpWage("Airtel",40,200,30);
-		Dmart.computeEmpWage();
-		Airtel.computeEmpWage();
-		empWage.add(Dmart);
-		empWage.add(Airtel);
+			totalEmpHrs += empHrs;
+			System.out.println("Days:" + totalWorkingDays + " Emp hrs" + empHrs);
+		}
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("total Emp Wage : =" + totalEmpWage);
+		return totalEmpWage;
 	}
 }
 
+//DMART COMPANY HAVING ITS OWN LOGIC AND DATA FOR ITS EMPLOYEE WAGE 
+class Dmart implements CompanyEmpWage {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 30;
+	public static final int NUM_OF_WORKING_DAYS = 5;
+	public static final int MAX_HRS_IN_MONTH = 20;
+	
+	//IMPLEMENTATION DECLARED IN INTERFACE OF COMPPUTE_EMP_WAGE METHOD FOR COMPANY OWN PURPOSE
+	@Override
+	public int computeEmpWage() {
+		// variable
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		System.out.println("            Wellcome to Dmart wage calculater     ");
+		System.out.println("----------------------------------------------------");
+		// COMPUTATION
+		while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
+			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+			switch (empCheck) {
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			default:
+				empHrs = 4;
+			}
+			totalEmpHrs += empHrs;
+			System.out.println("Days:" + totalWorkingDays + " Emp hrs" + empHrs);
+		}
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("total Emp Wage : =" + totalEmpWage);
+		return totalEmpWage;
+
+	}
+
+}
+
+//MAIN CLASS OR CONTROLER CLASS
+public class EmpWageBuilder {
+	public static void main(String args[]) {
+		Airtel airtel = new Airtel();
+		airtel.computeEmpWage();
+		Dmart dmart = new Dmart();
+		dmart.computeEmpWage();
+	}
+}
